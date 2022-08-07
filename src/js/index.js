22033cacbc14faf31036d36fce3662b36ace1bf4
Aselemi Divine <aselemidivine@gmail.com>
2022-08-08 00:47:33 +0100

    Gave a comment to add input form for next version

--- src/js/index.js
+++ src/js/index.js
@@ -136,4 +136,4 @@ window.addEventListener('click', clearModalResult);
 restart.addEventListener('click', restartGame);
 
 
-// input from will be included in the next version
+// input form will be included in the next version
